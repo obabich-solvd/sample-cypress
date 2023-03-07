d7e768c9cb8eb5938f1361f233856ec721e490e1
obabich-solvd <71430326+obabich-solvd@users.noreply.github.com>
2023-03-07 19:45:16 +0300

    fixes

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -2,26 +2,3 @@ require('@zebrunner/javascript-agent-cypress/lib/commands/commands');
 require('cypress-xpath');
 
 import 'cypress-wait-until';
-
-Cypress.Commands.add('findElement', (searchContext, locator) => {
-  return cy.wrap(searchContext).find(locator);
-});
-
-Cypress.Commands.add('getText', (locator) => {
-  return cy.get(locator).invoke('text');
-});
-
-Cypress.Commands.add('clearField', (locator) => {
-  return cy.getElement(locator).clear();
-});
-
-Cypress.Commands.add('getElement', (locator) => {
-  cy.waitUntil(() =>
-    cy
-      .get(locator)
-      .should('be.visible')
-      .then((element) => {
-        return element;
-      })
-  );
-});
