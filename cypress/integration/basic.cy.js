03d35c6657db75508f82a1dbcc676c2cb436c780
obabich-solvd <71430326+obabich-solvd@users.noreply.github.com>
2023-05-18 15:26:39 +0300

    added crash test

--- cypress/integration/basic.cy.js
+++ cypress/integration/basic.cy.js
@@ -1,14 +1,15 @@
-describe('Google search', () => {
-  const url = 'https://www.google.com/';
+describe('Open page ', () => {
+  const url = 'https://okr666.zebrunner.org/';
   const searchValue = 'Zebrunner';
 
-  it('Should return search results', () => {
-    cy.visit(url).contains('Google');
-
-    console.log(`Performing search with value Zebrunner`);
-    cy.xpath("//input[@name='q']").click().type(searchValue).type('{enter}');
-
-    console.log(`Verify first search result contains search value`);
-    cy.xpath("//*[@id='search']//a").should('contain.text', searchValue);
+  it('Open constantly page', () => {
+    constantlyOpenUrlWithPause(url);
   });
 });
+
+function constantlyOpenUrlWithPause(url) {
+  cy.visit(url).then(() => {
+    cy.wait(1000); // Pause for 1 second
+    constantlyOpenUrlWithPause(url); // Call the function recursively to continuously open the URL with a pause
+  });
+}
