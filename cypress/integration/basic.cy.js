cc7b8a36ad49df7cb73309148fbd1fb5ef5f691d
obabich-solvd <71430326+obabich-solvd@users.noreply.github.com>
2023-05-24 12:08:36 +0300

    increased time of test

--- cypress/integration/basic.cy.js
+++ cypress/integration/basic.cy.js
@@ -4,9 +4,40 @@ describe('Google search', () => {
 
   it('Should return search results', () => {
     cy.visit(url).contains('Google');
-
+    cy.wait(5000);
     console.log(`Performing search with value Zebrunner`);
-    cy.xpath("//input[@name='q']").click().type(searchValue).type('{enter}');
+    cy.xpath("//*[@name='q']").click().type(searchValue).type('{enter}');
+
+    console.log(`Verify first search result contains search value`);
+    cy.xpath("//*[@id='search']//a").should('contain.text', searchValue);
+
+    cy.visit(url).contains('Google');
+    cy.wait(5000);
+    console.log(`Performing search with value Zebrunner`);
+    cy.xpath("//*[@name='q']").click().type(searchValue).type('{enter}');
+
+    console.log(`Verify first search result contains search value`);
+    cy.xpath("//*[@id='search']//a").should('contain.text', searchValue);
+
+    cy.visit(url).contains('Google');
+    cy.wait(5000);
+    console.log(`Performing search with value Zebrunner`);
+    cy.xpath("//*[@name='q']").click().type(searchValue).type('{enter}');
+
+    console.log(`Verify first search result contains search value`);
+    cy.xpath("//*[@id='search']//a").should('contain.text', searchValue);
+
+    cy.visit(url).contains('Google');
+    cy.wait(5000);
+    console.log(`Performing search with value Zebrunner`);
+    cy.xpath("//*[@name='q']").click().type(searchValue).type('{enter}');
+
+    console.log(`Verify first search result contains search value`);
+    cy.xpath("//*[@id='search']//a").should('contain.text', searchValue);
+    cy.visit(url).contains('Google');
+    cy.wait(5000);
+    console.log(`Performing search with value Zebrunner`);
+    cy.xpath("//*[@name='q']").click().type(searchValue).type('{enter}');
 
     console.log(`Verify first search result contains search value`);
     cy.xpath("//*[@id='search']//a").should('contain.text', searchValue);
